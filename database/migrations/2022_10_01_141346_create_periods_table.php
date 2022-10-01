b5e96ec1f803c0d2af2a46074f35640bdc8b49a1
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-01 15:44:23 +0100

    migrations: create periods table

---
+++ database/migrations/2022_10_01_141346_create_periods_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('ends_at');
+            $table->string('name', 40)->nullable();
+            $table->float('price')->nullable()->unsigned();
+            $table->timestamp('starts_at');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('country_id')->constrained();
+            $table->foreignId('currency_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->unique(['starts_at', 'user_id']);
+            $table->unique(['ends_at', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('periods');
+    }
+};
