2263b36718678d50ebfaf5ab3fbd481ee1012ca6
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-07 00:47:01 +0100

    banners

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,2 +1,7 @@
 <x-guest-layout>
+    <div class="bg-lime-300 container h-screen max-w-full mx-auto">
+        <div class="w-screen h-screen">
+            <img src="{{ Vite::asset('resources/images/homepage/two-people-sitting-in-their-lounge.jpeg') }}">
+        </div>
+    </div>
 </x-guest-layout>
