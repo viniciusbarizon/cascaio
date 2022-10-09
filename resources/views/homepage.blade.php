de6232985e811f449010f8acf66ba676be278f98
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-09 22:37:52 +0100

    logo

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,5 @@
 <x-guest-layout>
     <div class="bg-lime-300 container h-screen max-w-full mx-auto">
+        <img src="{{ Vite::asset('resources/images/logo.png') }}">
     </div>
 </x-guest-layout>
