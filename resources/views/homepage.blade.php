7e540dbf9f6be2e575d3d6f26ccdd2ef4348490b
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-10 19:36:00 +0100

    homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,11 @@
 <x-guest-layout>
-    <div class="grid h-screen max-w-full mx-auto place-items-center">
-        <div class="max-w-screen-xl">
-            <img src="{{ Vite::asset('resources/images/logo.png') }}">
+    <div class="bg-lime-400 flex flex-col h-screen max-w-full place-items-center text-black">
+        <div>
+            <img src="{{ Vite::asset('resources/images/logo/black.png') }}">
+        </div>
+
+        <div>
+            {!! _('Te ajudamos <b>gratuitamente</b> a organizar suas despesas.') !!}
         </div>
     </div>
 </x-guest-layout>
