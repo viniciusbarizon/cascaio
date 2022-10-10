904faaef5c7cd943fb2358388d98ce4bd9be9a7f
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-10 23:49:10 +0100

    homepage: logo

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,17 @@
 <x-guest-layout>
-    <div class="bg-lime-400 flex flex-col h-screen items-center justify-center max-w-full text-black">
-        <div>
-            <img alt="Logo" src="{{ Vite::asset('resources/images/logo/black.png') }}">
+    <div class="bg-lime-400 flex flex-col gap-4 h-screen items-center justify-center max-w-full px-4 text-black">
+        <div class="flex flex-row gap-4">
+            <div>
+                <img alt="Logo" src="{{ Vite::asset('resources/images/logo/black/elephant.png') }}">
+            </div>
+
+            <div>
+                <img alt="Logo" src="{{ Vite::asset('resources/images/logo/black/cascaio.png') }}">
+            </div>
         </div>
 
-        <div>
-            {{ __('Te ajudamos') }} <b>{{ __('gratuitamente') }}</b> {{ __('a organizar suas despesas.') }}
+        <div class="text-center text-lg">
+            {{ __('Te ajudamos') }} <b>{{ __('gratuitamente') }}</b> {{ __('a organizar suas despesas') }}.
         </div>
     </div>
 </x-guest-layout>
