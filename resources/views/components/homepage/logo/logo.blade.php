fac0af3f3a8d46efe0157e34a4bc5ecc9517382f
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-13 23:03:59 +0100

    x-homepage.logo.logo

---
+++ resources/views/components/homepage/logo/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-row gap-4">
+    <x-homepage.logo.image alt="Elephant" path="elephant/background-transparent.png"/>
+    <x-homepage.logo.image alt="Cascaio" path="cascaio.png"/>
+</div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,14 +1,8 @@
 <x-guest-layout>
     <div class="bg-lime-400 flex flex-col gap-8 h-screen items-center justify-center max-w-full px-4 text-black">
-        <div class="flex flex-row gap-4">
-            <x-homepage.logo.image alt="Elephant" path="elephant/background-transparent.png"/>
-            <x-homepage.logo.image alt="Cascaio" path="cascaio.png"/>
-        </div>
-
+        <x-homepage.logo.logo/>
         <x-homepage.we-help-you/>
-
         <x-homepage.login-and-register/>
-
         <x-homepage.talk-to-us email="cascaio@cascaio.com" text="Fale com a gente"/>
     </div>
 </x-guest-layout>
