af19af683fb7e9fb67fc849357f9833bfeaf7302
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-10-13 20:54:03 +0100

    components: homepage.talk-to-us

---
+++ resources/views/components/homepage/talk-to-us.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <b>{{ __('Fale com a gente') }}:</b>
+    <a class='hover:text-gray-600' href='mailto:cascaio@cascaio.com'>cascaio@cascaio.com</a>.
+</div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -22,9 +22,6 @@
             <x-homepage.button route="register" text="Registrar"/>
         </div>
 
-        <div>
-            <b>{{ __('Fale com a gente') }}:</b>
-            <a class='hover:text-gray-600' href='mailto:cascaio@cascaio.com'>cascaio@cascaio.com</a>.
-        </div>
+        <x-homepage.talk-to-us/>
     </div>
 </x-guest-layout>
