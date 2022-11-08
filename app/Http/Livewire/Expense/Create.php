d94ccc3ca987d422dc322bcc13ffd230ba19c43f
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-11-08 20:07:18 +0000

    livewire: expense.create

---
+++ app/Http/Livewire/Expense/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Expense;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.expense.create');
+    }
+}
