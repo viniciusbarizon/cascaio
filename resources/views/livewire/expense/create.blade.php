eb501f9233668b5c45edcc6ece3dc840c121ec64
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-11-08 21:05:32 +0000

    livewire: expense.create

--- resources/views/livewire/expense/create.blade.php
+++ resources/views/livewire/expense/create.blade.php
@@ -1,5 +1,16 @@
-<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-    <form>
+<div class="bg-white border-b border-gray-200 p-6 sm:px-20">
+    <h2 class="font-semibold mb-4">
+        {{ _('Nova despesa')}}
+    </h2>
 
+    <form class="text-sm">
+        <label class="block">
+            <span>
+                {{ _('Descrição') }}
+            </span>
+
+            <input class="mt-1 block w-full rounded-md border-lime-400 shadow-sm focus:border-lime-400 focus:ring
+                focus:ring-lime-400 focus:ring-opacity-50" type="text">
+        </label>
     </form>
 </div>
