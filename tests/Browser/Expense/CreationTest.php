af31d70150b73e0ba2b88cee1999ab1ec477d144
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2022-11-21 23:45:14 +0000

    dusk: create expense

--- tests/Browser/Expense/CreationTest.php
+++ tests/Browser/Expense/CreationTest.php
@@ -9,15 +9,25 @@ class CreationTest extends DuskTestCase
 {
     const INPUT_CATEGORY_NAME = 'category';
 
+    const INPUT_CATEGORY_DUSK = '@'.self::INPUT_CATEGORY_NAME;
+
     const INPUT_COUNTRY_NAME = 'country_id';
+
+    const INPUT_COUNTRY_DUSK = '@'.self::INPUT_COUNTRY_NAME;
 
     const INPUT_CREATE_NAME = 'create-expense';
 
     const INPUT_DESCRIPTION_NAME = 'description';
 
+    const INPUT_DESCRIPTION_DUSK = '@'.self::INPUT_DESCRIPTION_NAME;
+
     const INPUT_MADE_AT_NAME = 'made_at';
 
+    const INPUT_MADE_AT_DUSK = '@'.self::INPUT_MADE_AT_NAME;
+
     const INPUT_PRICE_NAME = 'price';
+
+    const INPUT_PRICE_DUSK = '@'.self::INPUT_PRICE_NAME;
 
     public function testCreateExpense()
     {
@@ -27,26 +37,26 @@ class CreationTest extends DuskTestCase
                 ->assertSee(_('Nova despesa'))
 
                 ->assertInputPresent(self::INPUT_CATEGORY_NAME)
-                ->assertAttribute('@'.self::INPUT_CATEGORY_NAME, 'maxlength', 50)
-                ->assertAttribute('@'.self::INPUT_CATEGORY_NAME, 'wire:model.lazy', self::INPUT_CATEGORY_NAME)
-
-                ->assertInputPresent(self::INPUT_DESCRIPTION_NAME)
-                ->assertAttribute('@'.self::INPUT_DESCRIPTION_NAME, 'type', 'text')
-                ->assertAttribute('@'.self::INPUT_DESCRIPTION_NAME, 'wire:model.lazy', self::INPUT_DESCRIPTION_NAME)
-
-                ->assertInputPresent(self::INPUT_MADE_AT_NAME)
-                ->assertAttribute('@'.self::INPUT_MADE_AT_NAME, 'type', 'date')
-                ->assertAttribute('@'.self::INPUT_MADE_AT_NAME, 'wire:model.lazy', self::INPUT_MADE_AT_NAME)
+                ->assertAttribute(self::INPUT_CATEGORY_DUSK, 'maxlength', 50)
+                ->assertAttribute(self::INPUT_CATEGORY_DUSK, 'wire:model.lazy', self::INPUT_CATEGORY_NAME)
 
                 ->assertInputPresent(self::INPUT_COUNTRY_NAME)
                 ->assertSelectHasOptions(self::INPUT_COUNTRY_NAME, [1, 2])
-                ->assertAttribute('@'.self::INPUT_COUNTRY_NAME, 'wire:model', self::INPUT_COUNTRY_NAME)
+                ->assertAttribute(self::INPUT_COUNTRY_DUSK, 'wire:model', self::INPUT_COUNTRY_NAME)
+
+                ->assertInputPresent(self::INPUT_DESCRIPTION_NAME)
+                ->assertAttribute(self::INPUT_DESCRIPTION_DUSK, 'type', 'text')
+                ->assertAttribute(self::INPUT_DESCRIPTION_DUSK, 'wire:model.lazy', self::INPUT_DESCRIPTION_NAME)
+
+                ->assertInputPresent(self::INPUT_MADE_AT_NAME)
+                ->assertAttribute(self::INPUT_MADE_AT_DUSK, 'type', 'date')
+                ->assertAttribute(self::INPUT_MADE_AT_DUSK, 'wire:model.lazy', self::INPUT_MADE_AT_NAME)
 
                 ->assertInputPresent(self::INPUT_PRICE_NAME)
-                ->assertAttribute('@'.self::INPUT_PRICE_NAME, 'type', 'number')
-                ->assertAttribute('@'.self::INPUT_PRICE_NAME, 'min', '0.01')
-                ->assertAttribute('@'.self::INPUT_PRICE_NAME, 'step', '0.01')
-                ->assertAttribute('@'.self::INPUT_PRICE_NAME, 'wire:model.lazy', self::INPUT_PRICE_NAME)
+                ->assertAttribute(self::INPUT_PRICE_DUSK, 'type', 'number')
+                ->assertAttribute(self::INPUT_PRICE_DUSK, 'min', '0.01')
+                ->assertAttribute(self::INPUT_PRICE_DUSK, 'step', '0.01')
+                ->assertAttribute(self::INPUT_PRICE_DUSK, 'wire:model.lazy', self::INPUT_PRICE_NAME)
 
                 ->assertInputPresent(self::INPUT_CREATE_NAME)
                 ->assertInputValue(self::INPUT_CREATE_NAME, _('Criar'));
